950f20e4fc278f881441185a5faa0c4cec37df8d
LunarMarathon <lmaytan24@gmail.com>
2023-02-04 13:20:45 +0530

    Added nouns, save text, get new text func
    random noun, saving text in an array, rendering new text btn

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 import { verbs } from "./verbs.js"
+import { nouns } from "./nouns.js"
 
 const verb = document.getElementById("verb")
+const noun = document.getElementById("noun")
+const saveTextBtn = document.getElementById("saveTextBtn")
+const casualText = document.getElementById("casualText")
+const getTextBtn = document.getElementById("getTextBtn")
 
-let randomIndex = Math.floor(Math.random() * verbs.length)
-verb.textContent = verbs[randomIndex]
+renderText()
+
+function renderText(){
+    let randomVerbIndex = Math.floor(Math.random() * verbs.length)
+    verb.textContent = verbs[randomVerbIndex]
+    let randomNounIndex = Math.floor(Math.random() * nouns.length)
+    noun.textContent = nouns[randomNounIndex]
+}
+
+
+let savedTextArray = []
+
+saveTextBtn.addEventListener("click", function(){
+    console.log(casualText.textContent)
+    savedTextArray.push(casualText.textContent)
+    console.log(savedTextArray)
+})
+
+getTextBtn.addEventListener("click", renderText)
